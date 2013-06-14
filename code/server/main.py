a204931481f3a42819cd4f95f513ce0b2db4d9b0
max <EMAIL@ADRESSE.de>
2013-06-14 13:38:30 +0200

    Add proxy server and change http server.

--- code/server/main.py
+++ code/server/main.py
@@ -1,16 +1,28 @@
+from http import get_http_server
+from proxy import get_proxy_server
 from logging_wrapper import *
-from resource import ResourceServerHandler
-from BaseHTTPServer import HTTPServer
+import time
 
-def launch_server():
+HTTP_PORT = 8345
+PROXY_PORT = 8346
+
+def launch_servers():
+    http_server = get_http_server(HTTP_PORT)
+    proxy_server = get_proxy_server(PROXY_PORT)
+    
+    http_server.daemon = True
+    proxy_server.daemon = True
+
     try:
-        server = HTTPServer(('', 8345),
-                            ResourceServerHandler)
-        info("Started HTTP-Server")
-        server.serve_forever()
-    except KeyboardInterrupt:
-        info("Ctrl-C received, shut down")
-        server.socket.close()
+        info("Start HTTP-Server")
+        http_server.start()
+        info("Start Proxy-Server")
+        proxy_server.run() #should be proxy_server.start()
+        #while True:
+        #    time.sleep(10)
+    except KeyboardInterrupt:#caught in proxy_server...
+        info("Quit!")
+    pass
 
 if __name__ == "__main__":
-    launch_server()
+    launch_servers()
