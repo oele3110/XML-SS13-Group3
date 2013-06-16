657f385ba7a02b927b572fa60d61bd8f7ddefdc8
grossermax <Max@Maximilians-MacBook-Air.local>
2013-06-17 00:46:37 +0200

    Revert "syntax change"
    This reverts commit 67f5be6f0b42ad259f0866e66264523eead34e01.

--- code/parser/json/index.php
+++ code/parser/json/index.php
@@ -1,6 +1,6 @@
 <?php
 ini_set('display_errors', 1);
-require_once('twitterAPIExchange.php');
+require_once('TwitterAPIExchange.php');
 
 // Set access tokens here - see: https://dev.twitter.com/apps/
 $settings = array(
