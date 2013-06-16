940457a3655b29eb731083d3976bdc18ae093442
Dennis Lampert <oele3110@hotmail.com>
2013-06-16 20:57:27 +0200

    parser von microdata in rdf + sample output
    - einfacher python parser für microdata
    - funktioniert für stackoverflow
    - bekommt ein html file mit microdata als input
    - gibt ein rdf file aus
    - starten:
    python main.py inputFile.htm outputFile.rdf

--- code/parser/microdata/main.py
+++ code/parser/microdata/main.py
@@ -9,16 +9,27 @@ def openWriteFile(file):
 	f = open(file,'w')
 	return f
 
+def initRdf():
+	return '<?xml version="1.0"?>\n<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">'
+
+def initXml():
+	return '<?xml version="1.1" encoding="UTF-8" standalone="yes"?>\n'
+
 def parse(inputFile, outputFile):
 	
 	#inputFile = 'sample.htm'
 	#outputFile = 'output.xml'
 	
 	f = openReadFile(inputFile)
-	output = ''
+	
+	output = initXml()
+	outputRdf = initRdf()
+	
 	matchDescr = False
+	strMatch4 = ''
 	match6a = False
 	match7a = False
+	
 	
 	for line in f:
 		pattern1 = '\s*<div (itemscope) (itemtype)="(.*)">'
@@ -47,15 +58,18 @@ def parse(inputFile, outputFile):
 		
 		# itemscope itemtype
 		if matchObj1:
-			output += '<?xml version="1.1" encoding="UTF-8" standalone="yes"?>\n<itemscope itemtype="' + matchObj1.group(3) + '">\n'
+			output += '<itemscope itemtype="' + matchObj1.group(3) + '">\n'
+			outputRdf += '\n\t<itemscope>\n\t\t<rdf:Description rdf:about="' + matchObj1.group(3) + '">'
 		
 		# itemprop = image
 		if matchObj2:
 			output += '\t<itemprop type="' + matchObj2.group(2) + '">\n\t\t<href>' + matchObj2.group(4) + '</href>\n\t</itemprop>\n'
+			outputRdf += '\n\t\t\t<itemprop>\n\t\t\t\t<rdf:Description rdf:about="' + matchObj2.group(2) + '">\n\t\t\t\t\t<href>' +  matchObj2.group(4) + '</href>\n\t\t\t\t</rdf:Description>\n\t\t\t</itemprop>'
 		
 		# itemprop = name
 		if matchObj3:
 			output += '\t<itemprop type="' + matchObj3.group(2) + '">\n\t\t<content>' + matchObj3.group(3) + '</content>\n\t</itemprop>\n'
+			outputRdf += '\n\t\t\t<itemprop>\n\t\t\t\t<rdf:Description rdf:about="' + matchObj3.group(2) + '">\n\t\t\t\t\t<' + matchObj3.group(2) + '>' + matchObj3.group(3) + '</' + matchObj3.group(2) + '>\n\t\t\t\t</rdf:Description>\n\t\t\t</itemprop>'
 		
 		# itemprop = description
 		if matchDescr:
@@ -63,32 +77,45 @@ def parse(inputFile, outputFile):
 			# <p>
 			if matchObj5:
 				output += '<p>' + matchObj5.group(1) + "</p>\\n\\n"
+				outputRdf += '<p>' + matchObj5.group(1) + "</p>\\n\\n"
 			elif matchObj6a:
 				output += '<p>' + matchObj6a.group(1) + " "
+				outputRdf += '<p>' + matchObj6a.group(1) + " "
 			elif matchObj6b:
 				output += matchObj6b.group(1) + "</p>\\n\\n"
+				outputRdf += matchObj6b.group(1) + "</p>\\n\\n"
 			# <code>
 			elif matchObj7a:
 				match7a = True
 				output+= matchObj7a.group(1) + '\\n'
+				outputRdf += matchObj7a.group(1) + '\\n'
 			elif matchObj7b:
 				match7a = False
 				output += matchObj7b.group(1) + '\\n'
+				outputRdf += matchObj7b.group(1) + '\\n'
 			elif match7a:
 				output += re.match('(.*)',line).group(1) + '\\n'
+				outputRdf += re.match('(.*)',line).group(1) + '\\n'
 			# </div>
 			elif matchObj8:
 				matchDescr = False
 				output += '</content>\n\t</itemprop>\n'
+				outputRdf += '</' + strMatch4 + '>\n\t\t\t\t</rdf:Description>\n\t\t\t</itemprop>'
 		
 		if matchObj4:
 			matchDescr = True
+			strMatch4 = matchObj4.group(2)
 			output += '\t<itemprop type="' + matchObj4.group(2) + '">\n\t\t<content>'
+			outputRdf += '\n\t\t\t<itemprop>\n\t\t\t\t<rdf:Description rdf:about="' + matchObj4.group(2) + '">\n\t\t\t\t\t<' + matchObj4.group(2) + '>'
 
 	output += '</itemscope>'
+	outputRdf += '\n\t\t</rdf:Description>\n\t</itemscope>\n</rdf:RDF>'
 	
-	f = openWriteFile(outputFile)
-	f.write(output)
+	#f = openWriteFile(outputFile)
+	#f.write(output)
+	
+	f2 = openWriteFile(outputFile)
+	f2.write(outputRdf)
 
 
 def main():
