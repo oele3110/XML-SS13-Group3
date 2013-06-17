232317a17b02e264cc47dbd1014f9ebbf54be01d
grossermax <Max@Maximilians-MacBook-Air.local>
2013-06-17 21:02:08 +0200

    Parse from XML to RDF. But didn`t check RDF, yet.

--- code/parser/json/main.py
+++ code/parser/json/main.py
@@ -7,6 +7,7 @@ import json
 
 inputFile = 'sbahnberlin.json'
 outputFile = 'sbahnberlin.xml'
+outputFile2 = 'sbahnberlin.rdf'
 
 def openReadFile(file):
     f = open(file, 'r')
@@ -15,6 +16,12 @@ def openReadFile(file):
 def openWriteFile(file):
     f = open(file, 'w')
     return f
+
+def initXML():
+	return '<?xml version="1.1" encoding="UTF-8" standalone="yes"?>\n'
+
+def initRDF():
+	return '<?xml version="1.0"?>\n<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">\n\t'
 
 def execPHPScript(url, getfield):
     cmd = 'php index.php %s %s' % (url, getfield)
@@ -27,37 +34,53 @@ def parse(inputFile, outputFile):
     tmp = json.load(f)  # load data from file
     #data = json.dumps(tmp, sort_keys=True, indent=2) # use json.dumps because of possibility of using different options
 
-    # header
-    output = '<?xml version="1.1" encoding="UTF-8" standalone="yes"?>\n<tweet>\n\t'
+    output = initXML()
+    outputRdf = initRDF()
+    
+    # root
+    output += '<tweet>\n\t'
+    outputRdf += '<rdf:Description rdf:about="">\n\t\t<tweet>\n\t\t\t'
 
     # name
     output += '<user>\n\t\t<name>' + tmp[0]['user']['name'] + '</name>\n\t\t'
+    outputRdf += '<rdf:Description rdf:about="">\n\t\t\t\t<user>\n\t\t\t\t\t<rdf:Description rdf:about="">\n\t\t\t\t\t\t<name>' + tmp[0]['user']['name'] + '</name>\n\t\t\t\t\t\t'
     
     # twitter-name
     output += '<twitter_name>' + tmp[0]['user']['screen_name'] + '</twitter_name>\n\t\t'
+    outputRdf += '<twitter_name>' + tmp[0]['user']['screen_name'] + '</twitter_name>\n\t\t\t\t\t\t'
     
     # followers
     output += '<followers_count>' + str(tmp[0]['user']['followers_count']) + '</followers_count>\n\t\t'
+    outputRdf += '<followers_count>' + str(tmp[0]['user']['followers_count']) + '</followers_count>\n\t\t\t\t\t\t'
     
     # status
     output += '<statuses_count>' + str(tmp[0]['user']['statuses_count']) + '</statuses_count>\n\t' + '</user>\n\t'
+    outputRdf += '<statuses_count>' + str(tmp[0]['user']['statuses_count']) + '</statuses_count>\n\t\t\t\t\t</rdf:Description>\n\t\t\t\t' + '</user>\n\t\t\t\t'
 
     # retweets
     output += '<retweet_count>' + str(tmp[0]['retweet_count']) + '</retweet_count>\n\t'
+    outputRdf += '<retweet_count>' + str(tmp[0]['retweet_count']) + '</retweet_count>\n\t\t\t\t'
 
     # recent tweet
     output += '<text>' + tmp[0]['text'] + '</text>\n\t'
+    outputRdf += '<text>' + tmp[0]['text'] + '</text>\n\t\t\t\t'
 
     # date
     output += '<created_at>' + tmp[0]['created_at'] + '</created_at>\n'
+    outputRdf += '<created_at>' + tmp[0]['created_at'] + '</created_at>\n\t\t\t</rdf:Description>\n\t\t'
 
     # tail
     output += '</tweet>'
+    outputRdf += '</tweet>\n\t</rdf:Description>\n</rdf:RDF>'
 
     output = u''.join(output).encode('utf-8').strip()
+    outputRdf = u''.join(outputRdf).encode('utf-8').strip()
     
     f = openWriteFile(outputFile)
     f.write(str(output))
+
+    f2 = openWriteFile(outputFile2)
+    f2.write(str(outputRdf))
     
 def main():
     if len(sys.argv) != 2:
