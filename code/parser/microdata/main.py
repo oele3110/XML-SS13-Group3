d8060fc72d3f03e6d246b16b4682535c1da16531
Dennis Lampert <oele3110@hotmail.com>
2013-06-18 14:06:42 +0200

    modul integriert

--- code/parser/microdata/main.py
+++ code/parser/microdata/main.py
@@ -15,7 +15,7 @@ def initRdf():
 def initXml():
 	return '<?xml version="1.1" encoding="UTF-8" standalone="yes"?>\n'
 
-def parse(inputFile, outputFile):
+def parse2(inputFile, outputFile):
 	
 	#inputFile = 'sample.htm'
 	#outputFile = 'output.xml'
@@ -117,6 +117,94 @@ def parse(inputFile, outputFile):
 	f2 = openWriteFile(outputFile)
 	f2.write(outputRdf)
 
+def parse2(url, input):
+	
+	if !url:
+		url = ''
+	
+	outputRdf = initRdf()
+	
+	matchDescr = False
+	strMatch4 = ''
+	match6a = False
+	match7a = False
+	
+	
+	for line in input:
+		pattern1 = '\s*<div (itemscope) (itemtype)="(.*)">'
+		pattern2 = '\s*<link (itemprop)="(image)" (href)="(.*)"'
+		pattern3 = '\s*<h1 (itemprop)="(name)">(.*)</h1>'
+		pattern4 = '\s*<div .* (itemprop)="(description)">'
+		pattern5 = '\s*<p>(.*)</p>'
+		pattern6a = '\s*<p>(.*)'
+		pattern6b = '\s*(.*)</p>'
+		pattern7a = '\s*(<pre><code>.*)'
+		pattern7b = '\s*(</code></pre>)'
+		pattern8 = '\s*</div>'
+		
+		matchObj1 = re.match(pattern1, line)
+		matchObj2 = re.match(pattern2, line)
+		matchObj3 = re.match(pattern3, line)
+		matchObj4 = re.match(pattern4, line)
+		if matchDescr:
+			matchObj5 = re.match(pattern5, line)
+			matchObj6a = re.match(pattern6a, line)
+			matchObj6b = re.match(pattern6b, line)
+			matchObj7a = re.match(pattern7a, line)
+			matchObj7b = re.match(pattern7b, line)
+			matchObj8 = re.match(pattern8, line)
+		
+		
+		# itemscope itemtype
+		if matchObj1:
+			outputRdf += '\n\t<itemscope>\n\t\t<rdf:Description rdf:about="' + matchObj1.group(3) + '">'
+		
+		# itemprop = image
+		if matchObj2:
+			outputRdf += '\n\t\t\t<itemprop>\n\t\t\t\t<rdf:Description rdf:about="' + matchObj2.group(2) + '">\n\t\t\t\t\t<href>' +  matchObj2.group(4) + '</href>\n\t\t\t\t</rdf:Description>\n\t\t\t</itemprop>'
+		
+		# itemprop = name
+		if matchObj3:
+			outputRdf += '\n\t\t\t<itemprop>\n\t\t\t\t<rdf:Description rdf:about="' + matchObj3.group(2) + '">\n\t\t\t\t\t<' + matchObj3.group(2) + '>' + matchObj3.group(3) + '</' + matchObj3.group(2) + '>\n\t\t\t\t</rdf:Description>\n\t\t\t</itemprop>'
+		
+		# itemprop = description
+		if matchDescr:
+			
+			# <p>
+			if matchObj5:
+				outputRdf += '<p>' + matchObj5.group(1) + "</p>\\n\\n"
+			elif matchObj6a:
+				outputRdf += '<p>' + matchObj6a.group(1) + " "
+			elif matchObj6b:
+				outputRdf += matchObj6b.group(1) + "</p>\\n\\n"
+			# <code>
+			elif matchObj7a:
+				match7a = True
+				outputRdf += matchObj7a.group(1) + '\\n'
+			elif matchObj7b:
+				match7a = False
+				outputRdf += matchObj7b.group(1) + '\\n'
+			elif match7a:
+				outputRdf += re.match('(.*)',line).group(1) + '\\n'
+			# </div>
+			elif matchObj8:
+				matchDescr = False
+				outputRdf += '</' + strMatch4 + '>\n\t\t\t\t</rdf:Description>\n\t\t\t</itemprop>'
+		
+		if matchObj4:
+			matchDescr = True
+			strMatch4 = matchObj4.group(2)
+			outputRdf += '\n\t\t\t<itemprop>\n\t\t\t\t<rdf:Description rdf:about="' + matchObj4.group(2) + '">\n\t\t\t\t\t<' + matchObj4.group(2) + '>'
+
+	outputRdf += '\n\t\t</rdf:Description>\n\t</itemscope>\n</rdf:RDF>'
+	
+	return outputRdf
+
+def run(url, input):
+	inputArray = re.split("\n", input)
+	rdf = parse(url, inputArray)
+	return rdf
+	
 
 def main():
 	if len(sys.argv) != 3:
@@ -127,6 +215,6 @@ def main():
 		print 'input file: ' + inputFile
 		print 'output file: ' + outputFile
 		print 'start parsing file'
-		parse(inputFile, outputFile)
+		parse2(inputFile, outputFile)
 
-main()
+# main()
