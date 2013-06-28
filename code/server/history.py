f2bd20c361eddce716f605fe389e09674ad35fa1
oele3110 <oele3110@hotmail.com>
2013-06-28 22:19:42 +0200

    history adapted to new sparql return

--- code/server/history.py
+++ code/server/history.py
@@ -47,53 +47,7 @@ def getHistory():
 		html += "<p>" + key + " : <a href=\"" + dictionary[key] + "\">" + dictionary[key] + "</a></p>\n"
 	
 	html += buildFooter()
-	"""
-	f = open("history.html", "w")
-        f.write(html)
-        f.flush()
-        f.close()
-
 	
-	resource = None
-	timestamp = None
-	dictionary = None
-	
-	html = buildHeader()
-	
-	historyArray = re.split("\n", history)
-	
-	for line in historyArray:
-
-		matchObj1 = re.match(pattern1, line)
-		if matchObj1:
-			#print matchObj1.group(1)
-			resource = matchObj1.group(1)
-
-		matchObj2 = re.match(pattern2, line)
-		if resource and matchObj2:
-			#print matchObj2.group(1)
-			timestamp = matchObj2.group(1)
-			tempDict = { timestamp : resource}
-			if not dictionary:
-				dictionary = tempDict
-			else:
-				dictionary.update(tempDict)
-		
-		if resource and timestamp:
-			resource = None
-			timestamp = None
-	
-	for key in sorted(dictionary.iterkeys(), reverse = True):
-		#print key + "\t" + dictionary[key]
-		html += "<p>" + key + " : <a href=\"" + dictionary[key] + "\">" + dictionary[key] + "</a></p>\n"
-	
-	html += buildFooter()
-	
-	#f = open("history.html", "w")
-        #f.write(html)
-        #f.flush()
-        #f.close()
-	"""
 	return html
 
 getHistory()
