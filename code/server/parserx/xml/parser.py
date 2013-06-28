6c9097cc05a4e23b599570bbd5422dcf199cf2ff
max <EMAIL@ADRESSE.de>
2013-06-28 12:13:57 +0200

    Update xml xslt parser.

--- code/server/parserx/xml/parser.py
+++ code/server/parserx/xml/parser.py
@@ -6,15 +6,34 @@ XSLT_STYLESHEET = '''\
      xmlns="http://www.openarchives.org/OAI/2.0/"
      xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
      xmlns:oai_dc="http://www.openarchives.org/OAI/2.0/oai_dc/"
-     xmlns:dc="http://purl.org/dc/elements/1.1/">
+     xmlns:dc="http://purl.org/dc/elements/1.1/"
+     xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+     xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
+     xmlns:xsd="http://www.w3.org/2001/XMLSchema#">
 
-     <xsl:template match="ListRecords/*">
-         <xsl:copy-of select="." />
+     <xsl:template match="ListRecords/record/metadata">
+         <xsl:element name="rdf:Description">
+             <xsl:attribute name="rdf:about">
+                 <xsl:value-of select="oai_dc:dc/dc:identifier"/>
+             </xsl:attribute>
+             <xsl:text>&#xa;</xsl:text>
+             <xsl:for-each select="oai_dc:dc/*[text() and not (self::dc:identifier)]">
+                 <xsl:copy-of select="."/><xsl:text>&#xa;</xsl:text>
+             </xsl:for-each>
+         </xsl:element><xsl:text>&#xa;</xsl:text>
      </xsl:template>
 
+     <xsl:template match="/">
+         <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+         	xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
+         	xmlns:dc="http://purl.org/dc/elements/1.1/"
+         	xmlns:xsd="http://www.w3.org/2001/XMLSchema#"><xsl:text>&#xa;</xsl:text>
+              <xsl:apply-templates />
+         </rdf:RDF>
+     </xsl:template>
 
-    <xsl:template match="text()|@*">
-    </xsl:template>
+     <xsl:template match="text()|@*">
+     </xsl:template>
 
 </xsl:stylesheet>'''
 
@@ -35,11 +54,13 @@ def parse_string(s):
 
 
 if __name__ == "__main__":
-    f = open("entscholar_List_Records_min.xml")
-    #f = open("entscholar_List_Records.xml")
+    #f = open("first_record_input.xml")
+    #f = open("entscholar_List_Records_min.xml")
+    f = open("entscholar_List_Records.xml")
     s = f.read()
     f.close()
 
+    #bugfix
     s = s.replace("http://www.openarchives.org/OAI/2.0/", "", 1)
 
     out = parse_string(s)
