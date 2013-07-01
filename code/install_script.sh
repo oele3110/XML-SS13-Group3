e7a4df31142700e08a142dddc5628417111f1106
Marcin Nawrocki <Marcin.Nawrocki@fu-berlin.de>
2013-07-01 20:38:54 +0200

    install script for 4spot and sparql query added

---
+++ code/install_script.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/sh
+
+# get dependencies
+sudo apt-get install build-essential libpcre3-dev librasqal3-dev libtool libraptor1-dev libglib2.0-dev ncurses-dev libreadline-dev
+
+# get 4store
+tar -zxvf 4store-v1.1.5.tar.gz
+cd 4store-v1.1.5/
+
+# compile 4store
+export PKG_CONFIG_PATH=/usr/lib64/pkgconfig:/usr/share/pkgconfig:/usr/local/lib64/pkgconfig:/usr/local/share/pkgconfig
+./configure
+make
+
+# install and test 4store
+sudo make install
+make test
+echo -e '\nSome tests may have failed, but 4store is still good to go.\n'
+
+# set up 4store database
+4s-backend-setup xml
+
+# install sparql-query dependencies
+cd ..
+sudo apt-get install glib-networking libcurl3 libreadline6 libxml2
+
+# get sparql-query
+git clone https://github.com/tialaramex/sparql-query.git
+cd ./sparql-query
+
+# install sparql-query
+make all
+sudo make install
